39c6eed9f092047384e5904f89c09f6c780cb9b5
ehc0825 <ehc0825@naver.com>
2023-01-15 21:44:17 +0900

    done once finished

--- src/main/java/org/elasticsearch/plugin/analysis/processor/ImageToVectorProcessor.java
+++ src/main/java/org/elasticsearch/plugin/analysis/processor/ImageToVectorProcessor.java
@@ -18,12 +18,14 @@ import java.util.function.BiConsumer;
 public class ImageToVectorProcessor extends AbstractProcessor {
 
     private String field;
+    private String img_to_vec_api;
 
     public static final String TYPE = "url-image-to-vector";
 
-    protected ImageToVectorProcessor(String tag, String description, String field) {
+    protected ImageToVectorProcessor(String tag, String description, String field, String img_to_vec_api) {
         super(tag, description);
         this.field = field;
+        this.img_to_vec_api = img_to_vec_api;
     }
 
     public static final class Factory implements Processor.Factory {
@@ -31,7 +33,8 @@ public class ImageToVectorProcessor extends AbstractProcessor {
         public Processor create(Map<String, Processor.Factory> processorFactories, String tag,
                                 String description, Map<String, Object> config) throws Exception {
             String field = ConfigurationUtils.readStringProperty(TYPE, tag, config, "field");
-            return new ImageToVectorProcessor(tag,description,field);
+            String url = ConfigurationUtils.readStringProperty(TYPE,tag,config,"img_to_vec_api");
+            return new ImageToVectorProcessor(tag,description,field,url);
         }
     }
 
@@ -49,7 +52,7 @@ public class ImageToVectorProcessor extends AbstractProcessor {
         }
         AccessController.doPrivileged((PrivilegedAction<IngestDocument>) () ->{
             try {
-                document.setFieldValue(field,getImageVectorValue(document.getFieldValue(field,String.class)));
+                document.setFieldValue(field,getImageVectorValue(document.getFieldValue(field,String.class),this.img_to_vec_api));
                 return document;
             } catch (IOException e) {
                 System.out.println("e.getMessage() = " + e.getMessage());
@@ -59,8 +62,9 @@ public class ImageToVectorProcessor extends AbstractProcessor {
         return document;
     }
 
-    private List<String> getImageVectorValue(String fieldValue) throws IOException {
-        String[] vectorValue= ImageToVectorUtil.imageUrlToVector(fieldValue);
+
+    private List<String> getImageVectorValue(String fieldValue , String img_to_vec_api) throws IOException {
+        String[] vectorValue= ImageToVectorUtil.imageUrlToVector(fieldValue,img_to_vec_api);
         List<String> vectorList = new ArrayList<>();
         for(String vector : vectorValue)
         {
--- src/main/java/org/elasticsearch/plugin/analysis/util/ImageToVectorUtil.java
+++ src/main/java/org/elasticsearch/plugin/analysis/util/ImageToVectorUtil.java
@@ -1,50 +1,76 @@
 package org.elasticsearch.plugin.analysis.util;
 
-import org.elasticsearch.SpecialPermission;
-
 import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.security.AccessController;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ImageToVectorUtil {
 
-    private static final String URL_TO_VEC_API_URL = "http://192.168.249.1:29888/urlImagevector";
+    private static final String URL_TO_VEC_API_URL = "http://127.0.0.1:29888/urlImagevector";
     private static final String POST = "POST";
     private static final String CONTENT_TYPE = "application/json";
 
 
-    public static String[] imageUrlToVector(String imageUrl) throws IOException {
-        URL url = new URL(URL_TO_VEC_API_URL);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+    public static String[] imageUrlToVector(String imageUrl, String img_to_vec_api) throws IOException {
+        URL url;
+        url = getUrl(img_to_vec_api);
+
+        HttpURLConnection connection = getHttpURLConnection(url);
 
         String json="{\"image_url\" : \"" +imageUrl+"\"}";
-
-        connection.setRequestMethod(POST);
-        connection.setRequestProperty("Content-Type", CONTENT_TYPE);
-        connection.setDoOutput(true);
-        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(connection
-                .getOutputStream()));
-
-        bufferedWriter.write(json);
-        bufferedWriter.flush();
-        bufferedWriter.close();
+        sendRequest(connection, json);
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        StringBuffer stringBuffer = new StringBuffer();
-        String inputLine;
 
-        while ((inputLine = bufferedReader.readLine()) != null)  {
-            stringBuffer.append(inputLine);
-        }
-        bufferedReader.close();
+        StringBuffer stringBuffer = getStringBuffer(bufferedReader);
 
         String response = stringBuffer.toString();
 
         String[] vectors = parseStringToVectorArray(response);
         return vectors;
+    }
+
+    private static StringBuffer getStringBuffer(BufferedReader bufferedReader) throws IOException {
+        StringBuffer stringBuffer = new StringBuffer();
+
+        String inputLine;
+        while ((inputLine = bufferedReader.readLine()) != null)  {
+            stringBuffer.append(inputLine);
+        }
+        bufferedReader.close();
+        return stringBuffer;
+    }
+
+    private static HttpURLConnection getHttpURLConnection(URL url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod(POST);
+        connection.setRequestProperty("Content-Type", CONTENT_TYPE);
+        connection.setDoOutput(true);
+        return connection;
+    }
+
+    private static void sendRequest(HttpURLConnection connection, String json) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(connection
+                .getOutputStream()));
+        bufferedWriter.write(json);
+        bufferedWriter.flush();
+        bufferedWriter.close();
+    }
+
+    private static URL getUrl(String img_to_vec_api) throws MalformedURLException {
+        URL url;
+        if(img_to_vec_api.equals("")|| img_to_vec_api ==null)
+        {
+            url = new URL(URL_TO_VEC_API_URL);
+        }
+        else
+        {
+            url = new URL(img_to_vec_api);
+        }
+        return url;
     }
 
     private static String[] parseStringToVectorArray(String response) {
